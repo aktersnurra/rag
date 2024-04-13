3c59ed779e3b30ab4877ae94242c9df076df681a
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-04-13 13:54:58 +0200

    Fix tqdm logging

--- rag/upload.py
+++ rag/upload.py
@@ -7,9 +7,6 @@ from tqdm import tqdm
 
 from rag.retriever.retriever import Retriever
 
-log.remove()
-log.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-
 
 @click.command()
 @click.option(
@@ -22,10 +19,12 @@ def main(directory: str):
     log.info(f"Uploading pfs found in directory {directory}...")
     retriever = Retriever()
     pdfs = Path(directory).glob("**/*.pdf")
-    for path in tqdm(pdfs):
+    for path in tqdm(list(pdfs)):
         retriever.add_pdf(path=path)
 
 
 if __name__ == "__main__":
+    log.remove()
+    log.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
     load_dotenv()
     main()
